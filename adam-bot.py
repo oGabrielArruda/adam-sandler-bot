d9e8ed6582ae6a1b7e3128e34da94162cd8a7304
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-05-24 16:42:00 -0300

    add portuguese language

--- adam-bot.py
+++ adam-bot.py
@@ -15,14 +15,30 @@ wait_on_rate_limit_notify=True)
 
 FILE_NAME = 'last_seen.txt'
 
-def generate_joke():
-    url = 'https://official-joke-api.appspot.com/random_joke'
-    response = requests.get(url)    
+
+def select_language(text):    
+    if(text.count('@adamsandlerbot en') > 0):
+        return 'en'
+    if(text.count('@adamsandlerbot pt') > 0):
+        return 'pt'
+    return 'en'
+
+def generate_joke_by_lang(url, quest_field, answer_field):
+    headers = {'Accept': 'application/json'}
+    response = requests.get(url, headers = headers)
     resp_json = response.json()
-    quest = resp_json['setup']
-    answer = resp_json['punchline']
+    quest = resp_json[quest_field]
+    answer = resp_json[answer_field]
     formated_joke = quest + "\n\n" + answer
     return formated_joke
+
+def generate_joke(lang):
+    if(lang == 'pt'):
+        url = 'https://us-central1-kivson.cloudfunctions.net/charada-aleatoria'
+        return 'lá vai uma piada engraçada\n\n' + generate_joke_by_lang(url, 'pergunta', 'resposta')
+    elif(lang == 'en'):
+        url = 'https://official-joke-api.appspot.com/random_joke'
+        return 'there goes a funny joke \n\n' + generate_joke_by_lang(url, 'setup', 'punchline')
 
 def select_image_path():
     nmr = random.randint(1, 32)
@@ -43,28 +59,32 @@ def store_last_seen(FILE_NAME, last_seen_id):
 
 def is_not_reply(tweet):
     count = tweet.full_text.count('@')
-    return count == 1
+    return count < 3
 
 def reply_tweet(tweet, joke, img):
-    message = ' there goes a funny joke \n\n'
-    message = message + joke
-    status = '@' + tweet.user.screen_name + message
+    status = '@' + tweet.user.screen_name + ' ' + joke
     api.create_favorite(tweet.id)
     api.update_with_media(img, status, in_reply_to_status_id = tweet.id)
     store_last_seen(FILE_NAME, tweet.id)
-
+    print("Replied!")
 
 def bot_run():
-    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')
+    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')    
     for tweet in reversed(tweets):
         if is_not_reply(tweet):
-            joke = generate_joke()
+            lang = select_language(tweet.full_text)
+            joke = generate_joke(lang)
             img = select_image_path()
             reply_tweet(tweet, joke, img)
 
 def main():
     while True:
-        bot_run()
-        time.sleep(10)
-
+        try:
+            bot_run()
+            time.sleep(10)
+        except tweepy.TweepError:
+            time.sleep(60)
+        except StopIteration:
+            break
+        
 main()
